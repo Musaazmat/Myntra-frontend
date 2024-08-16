0bef21ed83fb7e2a298d208b82eb58d6a385bae5
Musaazmat <musaniazmat@gmail.com>
2024-08-16 23:58:12 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -4,16 +4,8 @@ import { defineConfig } from 'vite';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Myntra-frontend/",
   server:{
     host:true,
     port:5173,
-    proxy:{
-      '/api': {
-        target:'http://localhost:8080/items',
-        changeOrigin:true,
-        rewrite: ( path)=> path.replace(/^\/api/,''),
-      },
-    },
   },
 });
